99f3c292208dd7a203cdba9dea5477cc45d236f4
zharley18 <zackharley@gmail.com>
2017-04-12 22:52:36 -0400

    initial commit

---
+++ scripts/type.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+	Typed.new("#typed", {
+		strings: ["I develop software.", "I organize tech events.", "I teach programming."],
+		startDelay: 500,
+		backDelay: 2000,
+		contentType: 'text',
+		backSpeed: 30,
+		typeSpeed: 40,
+		loop: null
+	});
+});
